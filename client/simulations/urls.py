85291721e118204b4b32f4c821a8a54b3baeafda
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2022-01-17 10:12:57 +0000

    update url

--- client/simulations/urls.py
+++ client/simulations/urls.py
@@ -9,6 +9,11 @@ urlpatterns = [
         views.CellmlModelCreateView.as_view(),
         name='create_simulation',
     ),
-
+    
+    url(
+        r'^(?P<pk>\d+)/delete$',
+        views.SimulationDeleteView.as_view(),
+        name='simulation_delete',
+    ),
 ]
 app_name = 'simulations'
