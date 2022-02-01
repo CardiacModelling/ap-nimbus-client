b22916ff8cf1891fa808a65dbc882a4296df0703
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2022-02-01 09:56:29 +0000

    start testing update view

--- client/simulations/tests/test_views.py
+++ client/simulations/tests/test_views.py
@@ -28,9 +28,37 @@ class TestSimulationListView:
 #@pytest.mark.django_db
 #class TestSimulationCreateView:
 #
-#@pytest.mark.django_db
-#class TestSimulationEditView:
-#
+@pytest.mark.django_db
+class TestSimulationEditView:
+    def test_not_logged_in(self, client, user, simulation_range):
+        response = client.get('/simulations/%d/edit' % , simulation_range.pk)
+        assert response.status_code == 302
+        
+    def test_non_owner_cannot_edit(self, other_user, client, simulation_range):
+        client.login(username=other_user.email, password='password')
+        response = client.get('/simulations/%d/edit' % , simulation_range.pk)
+        assert response.status_code == 302
+
+    def test_cannot_duplicate_title(self, logged_in_user, client, simulation_range, o_hara_model):
+        assert simulation_range.author == logged_in_user
+        new_simulation = simulation_recipe.make(author=logged_in_user,  model=o_hara_model)
+        data = {'title': new_simulation.title, 'notes': 'new notes'}
+        response = client.get('/simulations/%d/edit' % , simulation_range.pk, data=data)
+        assert response.status_code == 302
+
+    def test_owner_can_edit(self, logged_in_user, client, simulation_range):
+        assert simulation_range.author == logged_in_user
+        data = {'title': 'new title', 'notes': 'new notes'}
+        response = client.get('/simulations/%d/edit' % , simulation_range.pk, data=data)
+        assert response.status_code == 302
+
+    def test_admin_can_edit(self, logged_in_admin, client, simulation_range):
+        assert simulation_range.author != logged_in_admin
+        data = {'title': simulation_range.title, 'notes': 'new notes'}
+        response = client.get('/simulations/%d/edit' % , simulation_range.pk, data=data)
+        assert response.status_code == 302
+
+
 @pytest.mark.django_db
 class TestSimulationResultView:
     def test_non_loged_in_cannot_see(self, user, client, simulation_range):
