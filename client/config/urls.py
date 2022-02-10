086ce942ba528e70b57f9641fbb058359c3f2b03
Dr Maurice Hendrix <52317399+MauriceHendrix@users.noreply.github.com>
2022-02-10 11:45:18 +0000

    Update dev.txt (#20)
    added python 3.10 testing and moved to django 4

--- client/config/urls.py
+++ client/config/urls.py
@@ -13,8 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include, re_path
 from django.contrib import admin
+from django.urls import include, re_path
 from django.views.generic import TemplateView
 
 
