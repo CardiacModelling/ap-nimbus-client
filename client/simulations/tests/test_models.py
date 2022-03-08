98d0589c8ed82951c32091204620be4c85a9ac1a
MauriceHendrix <maurice.hendrix@nottingham.ac.uk>
2022-03-08 12:51:23 +0000

    simulation model coverage

--- client/simulations/tests/test_models.py
+++ client/simulations/tests/test_models.py
@@ -1,10 +1,11 @@
 import pytest
+import math
 from files.models import IonCurrent
 from simulations.models import CompoundConcentrationPoint, Simulation, SimulationIonCurrentParam
 
 
 @pytest.mark.django_db
-def test_simulation(simulation_range, simulation_points, simulation_pkdata, user, admin_user, other_user, o_hara_model):
+def test_simulation_negLogM(simulation_range, simulation_points, simulation_pkdata, user, admin_user, other_user, o_hara_model):
     assert str(simulation_range) == 'my simulation1'
     assert simulation_range.status == Simulation.Status.NOT_STARTED
     assert simulation_range.author == user
@@ -12,7 +13,68 @@ def test_simulation(simulation_range, simulation_points, simulation_pkdata, user
     assert simulation_range.pacing_frequency == 0.05
     assert simulation_range.maximum_pacing_time == 5
     assert simulation_range.ion_current_type == Simulation.IonCurrentType.PIC50
+    assert Simulation.conversion(simulation_range.ion_units)(10) == 10
     assert simulation_range.ion_units == Simulation.IonCurrentUnits.negLogM
+    assert simulation_range.pk_or_concs == Simulation.PkOptions.compound_concentration_range
+    assert simulation_range.minimum_concentration == 0
+    assert simulation_range.maximum_concentration == 100
+    assert simulation_range.intermediate_point_count == '4'
+
+    assert str(simulation_points) == 'my simulation2'
+    assert str(simulation_pkdata) == 'my simulation3'
+
+
+@pytest.mark.django_db
+def test_simulation_M(simulation_range, simulation_points, simulation_pkdata, user, admin_user, other_user, o_hara_model):
+    simulation_range.ion_current_type = Simulation.IonCurrentType.IC50
+    simulation_range.ion_units = Simulation.IonCurrentUnits.M
+    assert str(simulation_range) == 'my simulation1'
+    assert simulation_range.status == Simulation.Status.NOT_STARTED
+    assert simulation_range.author == user
+    assert simulation_range.model == o_hara_model
+    assert simulation_range.pacing_frequency == 0.05
+    assert simulation_range.maximum_pacing_time == 5
+    assert Simulation.conversion(simulation_range.ion_units)(10) == - math.log10(10)
+    assert simulation_range.pk_or_concs == Simulation.PkOptions.compound_concentration_range
+    assert simulation_range.minimum_concentration == 0
+    assert simulation_range.maximum_concentration == 100
+    assert simulation_range.intermediate_point_count == '4'
+
+    assert str(simulation_points) == 'my simulation2'
+    assert str(simulation_pkdata) == 'my simulation3'
+
+
+@pytest.mark.django_db
+def test_simulation_µM(simulation_range, simulation_points, simulation_pkdata, user, admin_user, other_user, o_hara_model):
+    simulation_range.ion_current_type = Simulation.IonCurrentType.IC50
+    simulation_range.ion_units = Simulation.IonCurrentUnits.µM
+    assert str(simulation_range) == 'my simulation1'
+    assert simulation_range.status == Simulation.Status.NOT_STARTED
+    assert simulation_range.author == user
+    assert simulation_range.model == o_hara_model
+    assert simulation_range.pacing_frequency == 0.05
+    assert simulation_range.maximum_pacing_time == 5
+    assert Simulation.conversion(simulation_range.ion_units)(10) == - math.log10(1e-6 * 10)
+    assert simulation_range.pk_or_concs == Simulation.PkOptions.compound_concentration_range
+    assert simulation_range.minimum_concentration == 0
+    assert simulation_range.maximum_concentration == 100
+    assert simulation_range.intermediate_point_count == '4'
+
+    assert str(simulation_points) == 'my simulation2'
+    assert str(simulation_pkdata) == 'my simulation3'
+
+
+@pytest.mark.django_db
+def test_simulation_nM(simulation_range, simulation_points, simulation_pkdata, user, admin_user, other_user, o_hara_model):
+    simulation_range.ion_current_type == Simulation.IonCurrentType.IC50
+    simulation_range.ion_units = Simulation.IonCurrentUnits.nM
+    assert str(simulation_range) == 'my simulation1'
+    assert simulation_range.status == Simulation.Status.NOT_STARTED
+    assert simulation_range.author == user
+    assert simulation_range.model == o_hara_model
+    assert simulation_range.pacing_frequency == 0.05
+    assert simulation_range.maximum_pacing_time == 5
+    assert Simulation.conversion(simulation_range.ion_units)(10) == - math.log10(1e-9 * 10)
     assert simulation_range.pk_or_concs == Simulation.PkOptions.compound_concentration_range
     assert simulation_range.minimum_concentration == 0
     assert simulation_range.maximum_concentration == 100
