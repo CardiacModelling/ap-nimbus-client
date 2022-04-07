8df7be75217589dd3da7ff9ad7cb70bf0d3d0758
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2022-04-07 16:51:45 +0000

    flake8 isort

--- client/config/develop_settings.py
+++ client/config/develop_settings.py
@@ -1,6 +1,29 @@
-from .base_settings import *
+import os
+
+from .production_settings import *  # noqa
+from .production_settings import BASE_DIR
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 ALLOWED_HOSTS = ['*']
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': False,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'core.context_processors.common',
+            ],
+        },
+    },
+]
+
 
