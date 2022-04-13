79a5d06d2398dd67519631e966692c845e680e0d
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2022-04-13 16:48:21 +0000

    flake8 isort

--- client/simulations/tests/test_views.py
+++ client/simulations/tests/test_views.py
@@ -2,8 +2,8 @@ import datetime
 import json
 import os
 import shutil
-import uuid
 import sys
+import uuid
 
 import httpx
 import pandas
@@ -756,15 +756,18 @@ class TestUpdate_unassigned:
 
     def test_no_val(self, unassigned_info):
         update_unassigned(unassigned_info, 'NoActionPotential_1')
-        unassigned_info == {'unassigned': False, 'max': sys.float_info.min, 'min': sys.float_info.max, 'min_scale': 1.5, 'max_scale': 1.5}
+        unassigned_info == {'unassigned': False, 'max': sys.float_info.min, 'min': sys.float_info.max,
+                            'min_scale': 1.5, 'max_scale': 1.5}
 
     def test_smaller(self, unassigned_info):
         update_unassigned(unassigned_info, -1.0e+201)
-        unassigned_info == {'unassigned': False, 'max': sys.float_info.min, 'min': sys.float_info.max, 'min_scale': 1.5, 'max_scale': 1.1}
+        unassigned_info == {'unassigned': False, 'max': sys.float_info.min, 'min': sys.float_info.max,
+                            'min_scale': 1.5, 'max_scale': 1.1}
 
     def test_larger(self, unassigned_info):
         update_unassigned(unassigned_info, 1.0e+201)
-        unassigned_info == {'unassigned': False, 'max': sys.float_info.min, 'min': sys.float_info.max, 'min_scale': 1.1, 'max_scale': 1.5}
+        unassigned_info == {'unassigned': False, 'max': sys.float_info.min, 'min': sys.float_info.max,
+                            'min_scale': 1.1, 'max_scale': 1.5}
 
     def test_normal_val(self, unassigned_info):
         update_unassigned(unassigned_info, -6)
