ac67b50f3623ec5791ae266858b7538fba0c89b0
Dr Maurice Hendrix <52317399+MauriceHendrix@users.noreply.github.com>
2022-04-22 16:29:46 +0100

    Update production_settings.py

--- client/config/production_settings.py
+++ client/config/production_settings.py
@@ -108,7 +108,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.['PGDATABASE'],
+        'NAME': os.environ['PGDATABASE'],
         'USER': os.environ['PGUSER'],
         'PASSWORD': os.environ['PGPASSWORD'],
         'HOST': os.environ['PGHOST'],
