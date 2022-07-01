8199361c725fef6d2d1961e38963bde1d06cae85
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2022-07-01 18:13:31 +0100

    PGUSER

--- client/config/production_settings.py
+++ client/config/production_settings.py
@@ -109,7 +109,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ['PGDATABASE'],
-        'USER': os.environ['POSTGRES_USER'],
+        'USER': os.environ['PGUSER'],
         'PASSWORD': os.environ['PGPASSWORD'],
         'HOST': os.environ['PGHOST'],
         'PORT': os.environ['PGPORT'],
