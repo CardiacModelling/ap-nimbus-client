10a90f9cb5f34c499f770511c9825989ee33c969
Dr Maurice Hendrix <52317399+MauriceHendrix@users.noreply.github.com>
2022-07-11 22:34:06 +0100

    added pythonpath variable (#66)

--- docker/kick_off.sh
+++ docker/kick_off.sh
@@ -3,4 +3,4 @@ python /opt/django/ap-nimbus-client/docker/create_database.py
 python /opt/django/ap-nimbus-client/client/manage.py migrate --noinput
 python /opt/django/ap-nimbus-client/client/manage.py collectstatic --noinput
 /etc/init.d/nginx restart
-uwsgi --ini /opt/django/ap-nimbus-client/docker/client_uwsgi.ini --logto2 /opt/django/media/uwsgi.log
+uwsgi --ini /opt/django/ap-nimbus-client/docker/client_uwsgi.ini
