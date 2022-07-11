143b56d92db86a3a11a32cc64ae1d454d002ec74
Dr Maurice Hendrix <52317399+MauriceHendrix@users.noreply.github.com>
2022-07-11 18:51:24 +0100

    Update Dockerfile (#63)
    * Update Dockerfile
    
    * Update kick_off.sh

--- docker/kick_off.sh
+++ docker/kick_off.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
-/opt/django/venv/bin/python /opt/django/ap-nimbus-client/docker/create_database.py
-/opt/django/venv/bin/python /opt/django/ap-nimbus-client/client/manage.py migrate --noinput
-/opt/django/venv/bin/python /opt/django/ap-nimbus-client/client/manage.py collectstatic --noinput
+python /opt/django/ap-nimbus-client/docker/create_database.py
+python /opt/django/ap-nimbus-client/client/manage.py migrate --noinput
+python /opt/django/ap-nimbus-client/client/manage.py collectstatic --noinput
 /etc/init.d/nginx restart
-/opt/django/venv/bin/uwsgi --ini /opt/django/ap-nimbus-client/docker/client_uwsgi.ini --logto2 /opt/django/media/uwsgi.log
+uwsgi --ini /opt/django/ap-nimbus-client/docker/client_uwsgi.ini --logto2 /opt/django/media/uwsgi.log
