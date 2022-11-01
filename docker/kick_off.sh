e1604f1710ffe2f6ebff172711136adabddd2b26
MauriceHendrix <maurice.hendrix@nottingham.ac.uk>
2022-11-01 14:10:45 +0000

    typo in dockerfile

--- docker/kick_off.sh
+++ docker/kick_off.sh
@@ -4,5 +4,5 @@ python /opt/django/ap-nimbus-client/client/manage.py migrate --noinput
 python /opt/django/ap-nimbus-client/client/manage.py collectstatic --noinput
 /etc/init.d/nginx restart
 uwsgi --ini /opt/django/ap-nimbus-client/docker/client_uwsgi.ini --daemonize /opt/django/media/uwsgi.log
-tial -f /opt/django/media/uwsgi.log
+tail -f /opt/django/media/uwsgi.log
 
