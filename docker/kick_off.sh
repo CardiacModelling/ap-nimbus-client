7651bd6e9be0ef0f3e1108987e72a417cdee66af
MauriceHendrix <maurice.hendrix@nottingham.ac.uk>
2022-11-01 11:03:14 +0000

    trying uwsgi as deamon

--- docker/kick_off.sh
+++ docker/kick_off.sh
@@ -3,4 +3,5 @@ python /opt/django/ap-nimbus-client/docker/create_database.py
 python /opt/django/ap-nimbus-client/client/manage.py migrate --noinput
 python /opt/django/ap-nimbus-client/client/manage.py collectstatic --noinput
 /etc/init.d/nginx restart
-uwsgi --ini /opt/django/ap-nimbus-client/docker/client_uwsgi.ini
+uwsgi --ini /opt/django/ap-nimbus-client/docker/client_uwsgi.ini --daemonize /opt/django/media/uwsgi.log
+
