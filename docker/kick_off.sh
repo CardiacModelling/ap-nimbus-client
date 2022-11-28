9f26d6d2a14e7045d1d23a4eaabf1756f3d6d48b
MauriceHendrix <maurice.hendrix@nottingham.ac.uk>
2022-11-28 16:43:14 +0000

    don't run portal as root

--- docker/kick_off.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-python /opt/django/ap-nimbus-client/docker/create_database.py
-python /opt/django/ap-nimbus-client/client/manage.py migrate --noinput
-python /opt/django/ap-nimbus-client/client/manage.py collectstatic --noinput
-/etc/init.d/nginx restart
-uwsgi --ini /opt/django/ap-nimbus-client/docker/client_uwsgi.ini
-
