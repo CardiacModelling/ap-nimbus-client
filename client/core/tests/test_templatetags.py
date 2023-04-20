d126b0d1c91f3d4e82f83a9c584ee1d05f037c7c
Dr Maurice Hendrix <52317399+MauriceHendrix@users.noreply.github.com>
2023-04-20 09:31:21 +0100

    Update test_templatetags.py

--- client/core/tests/test_templatetags.py
+++ client/core/tests/test_templatetags.py
@@ -15,3 +15,13 @@ def test_can_edit(user, other_user, admin_user, o_hara_model):
 @pytest.mark.django_db
 def test_hosting_info():
     return hosting_info() == settings.HOSTING_INFO
+
+
+@pytest.mark.django_db
+def test_contact_mailto():
+    return contact_mailto() == settings.CONTACT_MAILTO
+
+
+@pytest.mark.django_db
+def test_contact_text():
+    return contact_text() == settings.CONTACT_TEXT
