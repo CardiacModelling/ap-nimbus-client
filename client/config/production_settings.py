920e08c40f8616bbe74d0cb61fc2a35b0143438b
MauriceHendrix <maurice.hendrix@nottingham.ac.uk>
2023-04-26 11:07:48 +0000

    moving privacy notice to environment variable

--- client/config/production_settings.py
+++ client/config/production_settings.py
@@ -186,6 +186,9 @@ AP_PREDICT_STATUS_TIMEOUT = os.environ.get('AP_PREDICT_STATUS_TIMEOUT', 1000)
 # Hosting information for the privacy policy
 HOSTING_INFO = os.environ.get('HOSTING_INFO', '')
 
+#A brief statement that will be shown at the start of the privacy notice
+PIVACY_NOTICE = os.environ.get('PIVACY_NOTICE', '').replace('\\n', '<br/>')
+
 # Mailto link for contacting maintiners
 CONTACT_MAILTO = os.environ.get('CONTACT_MAILTO', '')
 
